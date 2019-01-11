59cbbd10c2e776129dc4c7652aaf693ea25e4f16
unknown <mikey@jigsaw.xyz>
2019-01-11 10:10:27 -0500

    post request workign

--- routes/workouts.js
+++ routes/workouts.js
@@ -1,8 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+const baseDir = path.join(__dirname, './../data/workouts');
 const express = require('express');
 const router = express.Router();
 const workouts = require('./../data/workouts/workouts.json');
 
 router.get('/', (req, res) => {
+  res.json(workouts);
+});
+
+router.post('/', (req, res) => {
+  fs.writeFile(baseDir + '/workouts.json', JSON.stringify(req.body), (err) => {
+    if (err) throw err;
+    console.log('saved!');
+  })
+
   res.json(workouts);
 });
 
